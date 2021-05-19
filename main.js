a7170ee0885285239bccd10391fc927478ce66d0
Devukth <78857657+Devukth@users.noreply.github.com>
2021-05-19 16:23:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 Webcam.set ({
-    width: 450,
-    height: 250,
+    width: 410,
+    height: 300,
     image_format: "png",
     png_quality: 90
 });
-camera = document.getElementById("camera");
-Webcam.attach("#camera");
+webcam = document.getElementById("webcam");
+Webcam.attach(webcam);
 
 console.log("ml5 version: " + ml5.version);
 var classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/LmlwFDNZM/model.json", modelLoaded);
@@ -17,16 +17,6 @@ function modelLoaded() {
 function identifyimage() {
     img = document.getElementById('photo');
     classifier.classify(img, getResult);
-}
-
-function getResult(error, results) {
-    if(error) {
-        console.error(error);
-    } else {
-        console.log(results);
-        document.getElementById('obj').innerHTML = results[0].label;
-        document.getElementById('accuracy').innerHTML = Number(results[0].confidence.toFixed(2)) * 100;
-    }
 }
 
 function captureImage() {
